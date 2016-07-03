9da866516f3a2d8705157cc3ada39c3baa8c19d2
zhanghao <jsyczhanghao@sina.com>
2016-07-03 18:43:50 +0800

    commit

--- index.js
+++ index.js
@@ -21,54 +21,16 @@ feather.config.merge({
         maxUrlLength: 2000
     },
     
-    cssA2R: false,
-
     template: {
         suffix: 'html',
         engine: 'feather'
     },
 
-    widget: {
-        rules: [        
-            /*
-            :nav => /widget/nav/nav.tpl
-            common:nav => /widget/common/nav/nav.tpl
-            common/a:nav => /widget/common/a/nav/nav.tpl
-            common/a:nav/a => /widget/common/a/nav/a/a.tpl
-            common/a:nav/a.tpl => /widget/common/a/nav/a.tpl
-            common/a/b => /widget/common/a/b.tpl
-            common/a/b.tpl => /widget/common/a/b/tpl
-            */
-            [/^([^:]+)?\:((?:[^\/]+\/)*)((?:(?!\.[^.]+).)+?)(\..+)?$/, function(_0, _1, _2, _3, _4){
-                return (_1 ? (_1 + '/') : '') + _2 + _3 + (_4 ? _4 : ('/' + _3 + '.' + feather.config.get('template.suffix')));
-            }]
-        ]
-    },
-
     statics: '/static',
 
     require: {
-        use: true,
         config: {
-            rules: [
-                /*
-                :dialog => /dialog/dialog.js
-                common:dialog => /common/dialog/dialog.js
-                common/a:dialog => /common/a/dialog/dialog.js
-                common/a:dialog/a => /common/a/dialog/a/a.js
-                common/a:dialog/a.js => /common/a/dialog/a.js
-                common:dialog.js => /common/dialog.js
-                common/a.js => /common/a.js
-                */
-
-                [/^([^:]+)?\:((?:[^\/]+\/)*)([^\.]+?)(\..+)?$/, function(_0, _1, _2, _3, _4){
-                    return (_1 ? _1 + '/' : '') + _2 + _3 + (_4 ? _4 : ('/' + _3 + '.js'));
-                }],
-
-                [/(?:^|\/)[^\.]+$/, function(all){
-                    return all + '.js';
-                }]
-            ]
+            rules: []
         }
     },
 
